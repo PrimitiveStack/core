be8dd1c04d1f8b834d1cf19983da61c00a0a9d1a
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-07-17 12:41:31 +0100

    revert: go back to previous commit

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: {
-    Nil: 'src/Nil.ts',
-    Result: 'src/Result.ts',
-  },
+  entry: ['./src/index.ts'],
   platform: 'neutral',
   dts: true,
   format: ['cjs', 'esm'],
